d1fd347ff929a205e9c82e2772a76e430613c521
RoniAksel <roniaksel@gmail.com>
2021-10-09 13:07:26 +0300

    first

---
+++ server/src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const { graphqlHTTP } = require('express-graphql');
+
+const schema = {
+  // we will add this later
+};
+
+app.use(cors());
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+    pretty: true
+  })
+);
+
+app.listen(4000);
+console.log('SERVER OK');
+
+// hlF9lKN6cue8Ry5y
